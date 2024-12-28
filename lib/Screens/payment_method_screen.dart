70fb4f6453d363f6cbcd7f53a462615ced139572
ShabanShaikhramjn <shabanshaikh72345@gmail.com>
2024-12-28 17:39:12 +0530

    payment 2.0

--- lib/Screens/payment_method_screen.dart
+++ lib/Screens/payment_method_screen.dart
@@ -49,13 +49,181 @@ body: SafeArea(
           Container(
             width: size.width,
             height: 55,
+            margin: EdgeInsets.only(right: 20),
             decoration: BoxDecoration(
               border: _type == 1 ? Border.all(width: 1, color: Colors.black): 
               Border.all(width: 0.3 , color: Colors.grey),
               borderRadius: BorderRadius.circular(5),
+              color: Colors.transparent
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Radio(value: 1, groupValue: _type, activeColor: Color(0xFFEF6969),
+                  onChanged: (val){},
+                   ),
+                   Text('Amazon Pay',
+                   style: _type ==1 ?
+                   TextStyle(fontSize: 15,
+                   fontWeight: FontWeight.bold,
+                   color: Colors.black)
+                   :
+                   TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                   ),
 
+                   
+                   
+                   ),
+                   
+                   Image.asset(
+                    'assets/amazon.png', 
+                    width: 100,
+                    height: 100,
+                   )
+                   
+                ],
+              ),
+            ),
+          ),
+  SizedBox(height: 20,),
+          Container(
+            width: size.width,
+            height: 55,
+            margin: EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              border: _type == 0 ? Border.all(width: 1, color: Colors.black): 
+              Border.all(width: 0.3 , color: Colors.grey),
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.transparent
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Radio(value: 1, groupValue: _type, activeColor: Color(0xFFEF6969),
+                  onChanged: (val){},
+                   ),
+                   Text('PayPal',
+                   style: _type ==1 ?
+                   TextStyle(fontSize: 15,
+                   fontWeight: FontWeight.bold,
+                   color: Colors.black)
+                   :
+                   TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                   ),
+
+                   
+                   
+                   ),
+                   
+                   Image.asset(
+                    'assets/paypal.png', 
+                    width: 100,
+                    height: 100,
+                   )
+                   
+                ],
+              ),
             ),
           )
+      ,  SizedBox(height: 20,),
+         
+         Container(
+            width: size.width,
+            height: 55,
+            margin: EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              border: _type == 0 ? Border.all(width: 1, color: Colors.black): 
+              Border.all(width: 0.3 , color: Colors.grey),
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.transparent
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Radio(value: 1, groupValue: _type, activeColor: Color(0xFFEF6969),
+                  onChanged: (val){},
+                   ),
+                   Text('Master card',
+                   style: _type ==1 ?
+                   TextStyle(fontSize: 15,
+                   fontWeight: FontWeight.bold,
+                   color: Colors.black)
+                   :
+                   TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                   ),
+
+                   
+                   
+                   ),
+                   
+                   Image.asset(
+                    'assets/master card.png', 
+                    width: 40,
+                    height: 40,
+                   )
+                   
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+
+           Container(
+            width: size.width,
+            height: 55,
+            margin: EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              border: _type == 0 ? Border.all(width: 1, color: Colors.black): 
+              Border.all(width: 0.3 , color: Colors.grey),
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.transparent
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Radio(value: 0, groupValue: _type, activeColor: Color(0xFFEF6969),
+                  onChanged: (val){},
+                   ),
+                   Text('Google Pay',
+                   style: _type ==1 ?
+                   TextStyle(fontSize: 15,
+                   fontWeight: FontWeight.bold,
+                   color: Colors.black)
+                   :
+                   TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                   ),
+
+                   
+                   
+                   ),
+                   
+                   Image.asset(
+                    'assets/google.jpg', 
+                    width: 100,
+                    height: 100,
+                   )
+                   
+                ],
+              ),
+            ),
+          )
+        
         ],
       ),
     ),
@@ -64,3 +232,7 @@ body: SafeArea(
     );
   }
 }
+
+
+
+// 2.21
